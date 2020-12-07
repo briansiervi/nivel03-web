dfc1c88eb5e3806f2e1a4e41b90dcf1a9aed32e5
brian <brianmazini@gmail.com>
2020-12-07 17:31:31 -0300

    layout da pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
+import SignIn from './pages/Signin/Index';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
