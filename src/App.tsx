582b1c4cfa164df5657c8870c58d055376ac35f1
Brian Mazini Siervi <brianmazini@gmail.com>
2020-12-12 11:38:41 -0300

    layout da signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/Signin/Index';
+import SignIn from './pages/SignIn/Index';
+import SignUp from './pages/SignUp/Index';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    {/* <SignIn /> */}
+    <SignUp />
     <GlobalStyle />
   </>
 );
